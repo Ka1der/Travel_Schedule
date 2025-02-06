ee3d0058b8446f2682ff31fddc332ab01003eb51
Kaider <kronoz@yandex.ru>
2025-02-06 21:15:14 +0300

    Initial Commit

---
+++ Travel_Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel_Schedule
+//
+//  Created by Kaider on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Travel_ScheduleTests/Travel_ScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleTests.swift
+//  Travel_ScheduleTests
+//
+//  Created by Kaider on 06.02.2025.
+//
+
+import Testing
+@testable import Travel_Schedule
+
+struct Travel_ScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
