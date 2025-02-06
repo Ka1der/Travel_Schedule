6c62b7424765dd97e970408b8260d94b85675251
Kaider <kronoz@yandex.ru>
2025-02-06 22:16:18 +0300

    Инициализация проекта и добавление SplashScreen

---
+++ Travel_Schedule/3_Views/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  Travel_Schedule
+//
+//  Created by Kaider on 06.02.2025.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Image("SplashScreen")
+            .resizable()
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
