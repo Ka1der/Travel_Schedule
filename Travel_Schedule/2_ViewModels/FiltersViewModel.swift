2d38597368eec8099d745bd391c13ae3ab98a730
Kaider <kronoz@yandex.ru>
2025-03-21 22:16:08 +0300

    Отрефакторил FiltresView на MVVM

--- Travel_Schedule/2_ViewModels/FiltersViewModel.swift
+++ Travel_Schedule/2_ViewModels/FiltersViewModel.swift
@@ -5,4 +5,37 @@
 //  Created by Kaider on 21.03.2025.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+final class FiltersViewModel: ObservableObject {
+    @Published var selectedTimes: Set<String> = []
+    @Published var showTransfers: Bool? = nil
+    
+    let timePeriods = [
+        "Утро 06:00 - 12:00",
+        "День 12:00 - 18:00",
+        "Вечер 18:00 - 00:00",
+        "Ночь 00:00 - 06:00"
+    ]
+    
+    var canApplyFilters: Bool {
+        return !selectedTimes.isEmpty && showTransfers != nil
+    }
+    
+    func toggleTimeSelection(for period: String) {
+        if selectedTimes.contains(period) {
+            selectedTimes.remove(period)
+        } else {
+            selectedTimes.insert(period)
+        }
+    }
+    
+    func setShowTransfers(value: Bool) {
+        showTransfers = value
+    }
+    
+    func applyFilters() {
+        // Логика применения фильтров
+    }
+}
